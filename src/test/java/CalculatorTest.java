dd839e168b49f54fe2f1a5aab221442db04caef7
Jacob McCullough <34502339+razorwired1124@users.noreply.github.com>
2023-11-04 07:00:54 +0000

    Added subtract, multiple, and divide tasks & tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -15,4 +15,34 @@ public class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testSubtract() {
+      int expected = 1;
+
+      int actual = calculator.subtract(3, 2);
+
+      assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testMultiply() {
+      int expected = 4;
+
+      int actual = calculator.multiply(2, 2);
+
+      assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDivide() {
+      int expected = 2;
+
+      int actual = calculator.divide(4, 2);
+
+      assertEquals(expected, actual);
+    }
+
+
+
 }
